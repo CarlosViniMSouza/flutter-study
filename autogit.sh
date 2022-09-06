8b3f90cc37db5cf9f55c624ce2b5f336c9af171a
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-09-05 22:39:18 -0300

    Project (step 01) -> finished

---
+++ autogit.sh
@@ -0,0 +1,9 @@
+git init
+git add .
+git commit -m "Project (step 01) -> finished"
+git branch -M main
+git remote add origin https://github.com/CarlosViniMSouza/flutter-study.git
+git push -u origin main
+git status
+
+# link: https://codelabs.developers.google.com/codelabs/first-flutter-app-pt2#0
